1309340f670b351745f03b31337e3179d813c090
Lucasus <lukasz.wiatrak@gmail.com>
2017-04-13 00:00:00 +0200

    Major redesign, Readme updated

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# gulp-replace [![NPM version][npm-image]][npm-url] [![Build status][travis-image]][travis-url]
+# gulp-replace-with-sourcemaps [![NPM version]][npm-url]
 > A string replace plugin for gulp 3
 
 ## Usage
 
-First, install `gulp-replace` as a development dependency:
+First, install `gulp-replace-with-sourcemaps` as a development dependency:
 
 ```shell
-npm install --save-dev gulp-replace
+npm install --save-dev gulp-replace-with-sourcemaps
 ```
 
 Then, add it to your `gulpfile.js`:
 
 ### Regex Replace
 ```javascript
-var replace = require('gulp-replace');
+var replace = require('gulp-replace-with-sourcemaps');
 
 gulp.task('templates', function(){
   gulp.src(['file.txt'])
@@ -21,33 +21,10 @@ gulp.task('templates', function(){
     .pipe(gulp.dest('build/file.txt'));
 });
 ```
-### String Replace
-```javascript
-var replace = require('gulp-replace');
-
-gulp.task('templates', function(){
-  gulp.src(['file.txt'])
-    .pipe(replace('bar', 'foo'))
-    .pipe(gulp.dest('build/file.txt'));
-});
-```
-
 
 ## API
 
-gulp-replace can be called with a string or regex.
-
-### replace(string, replacement[, options])
-
-#### string
-Type: `String`
-
-The string to search for.
-
-#### replacement
-Type: `String` or `Function`
-
-The replacement string or function. If `replacement` is a function, it will be called once for each match and will be passed the string that is to be replaced.
+gulp-replace-with-sourcemaps can be called with regex only.
 
 ### replace(regex, replacement[, options])
 
@@ -61,7 +38,7 @@ Type: `String` or `Function`
 
 The replacement string or function. See the [MDN documentation for String.replace] for details.
 
-### gulp-replace options
+### gulp-replace-with-sourcemaps options
 
 An optional third argument, `options`, can be passed.
 
@@ -74,11 +51,7 @@ Default: `false`
 
 Skip binary files
 
-
 [MDN documentation for RegExp]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
 [MDN documentation for String.replace]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Specifying_a_string_as_a_parameter
 
-[travis-url]: http://travis-ci.org/lazd/gulp-replace
-[travis-image]: https://secure.travis-ci.org/lazd/gulp-replace.svg?branch=master
-[npm-url]: https://npmjs.org/package/gulp-replace
-[npm-image]: https://badge.fury.io/js/gulp-replace.svg
+[npm-url]: https://npmjs.org/package/gulp-replace-with-sourcemaps
--- index.js
+++ index.js
@@ -6,14 +6,10 @@ var istextorbinary = require('istextorbinary');
 var applySourceMap = require('vinyl-sourcemaps-apply');
 var Replacer = require('regexp-sourcemaps');
 
-module.exports = function(search, replacement, options) {
+module.exports = function(search, replacementParameter, options) {
   return new Transform({
     objectMode: true,
     transform: function(file, enc, callback) {
-      if (options && file.sourceMap) {
-        options.makeSourceMaps = true;
-      }
-
       if (file.isNull()) {
         return callback(null, file);
       }
@@ -21,48 +17,31 @@ module.exports = function(search, replacement, options) {
       function doReplace() {
         
         if (file.isStream()) {
-          file.contents = file.contents.pipe(rs(search, replacement));
-          return callback(null, file);
+          throw new Error("Streams are not  supported");
         }
 
         if (file.isBuffer()) {
           if (search instanceof RegExp) {
-            var replacer = new Replacer(search,replacement);
-            var result = replacer.replace(file.contents.toString('utf8'), file.relative);
-           
-            file.contents = new Buffer(String(file.contents).replace(search, replacement));
+            var replacement;
+
+            if (typeof replacementParameter === 'function') {
+                replacement = replacementParameter(search);
+            } else {
+                replacement = replacementParameter;
+            }
+
+            var contentAfterReplacement = file.contents.toString('utf8').replace(search, replacement);
+            file.contents = new Buffer(replaced);
 
             if (file.sourceMap) {
+               var replacer = new Replacer(search,replacement);
+               var result = replacer.replace(contentAfterReplacement, file.relative);
                applySourceMap(file, result.map);
             }
+          } else {
+             throw new Error("Only regex replace is supported");
           }
-          else {
-            var chunks = String(file.contents).split(search);
-
-            var result;
-            if (typeof replacement === 'function') {
-              // Start with the first chunk already in the result
-              // Replacements will be added thereafter
-              // This is done to avoid checking the value of i in the loop
-              result = [ chunks[0] ];
-
-              // The replacement function should be called once for each match
-              for (var i = 1; i < chunks.length; i++) {
-                // Add the replacement value
-                result.push(replacement(search));
-
-                // Add the next chunk
-                result.push(chunks[i]);
-              }
-
-              result = result.join('');
-            }
-            else {
-              result = chunks.join(replacement);
-            }
-
-            file.contents = new Buffer(result);
-          }
+          
           return callback(null, file);
         }
 
